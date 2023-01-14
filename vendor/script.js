3edde03f43fa12a60c3f5c10ef608e962483bd62
Anna Radionova <aradion0va@yandex.ru>
2023-01-14 12:19:12 +0300

    3 prog popup

--- vendor/script.js
+++ vendor/script.js
@@ -1,7 +1,17 @@
 console.log("Hello!");
-function openForm() {
-    document.getElementById("myForm").style.display = "block"; }
-    
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+let popup = document.querySelector('.popup');
+popup.classList.toggle('popup_opened');
+
+let Edit_button = document.querySelector('.profile-info__Edit-button');
+Edit_button.addEventListener("click",OpenForm);
+
+function OpenForm() {
+    popup.classList.toggle('popup_opened');
+}
+
+let close = document.querySelector('.popup__container-close');
+close.addEventListener("click",CloseForm);
+
+function CloseForm() {
+    popup.classList.toggle('popup_opened');
 }

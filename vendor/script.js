78ef309a04f6cbdcbc8a1c75a0ae35fa8cc68e9f
Anna Radionova <aradion0va@yandex.ru>
2023-01-16 10:03:11 +0300

    3 proj

--- vendor/script.js
+++ vendor/script.js
@@ -15,3 +15,38 @@ close.addEventListener("click",CloseForm);
 function CloseForm() {
     popup.classList.toggle('popup_opened');
 }
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.container-input_Name');// Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.container-input_aboutYou')// Воспользуйтесь инструментом .querySelector()
+
+let container_save = document.querySelector('.popup__container-save');
+container_save.addEventListener("click",handleFormSubmit);
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    document.getElementById("nameInput").value;
+    document.getElementById("jobInput").value;// Получите значение полей jobInput и nameInput из свойства value
+
+    let title=document.querySelector('.profile-info__title');
+    let subtitle=document.querySelector('.profile-info__subtitle');
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    title.textContent = nameInput.value;
+    subtitle.textContent = jobInput.value;
+    // Вставьте новые значения с помощью textContent
+    let popup = document.querySelector('.popup');
+    popup.classList.toggle('popup_opened');
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
+

44467f1b7362f6d67f9ea02cab2ecf8e3b815a22
Anna Radionova <aradion0va@yandex.ru>
2023-01-25 12:39:44 +0300

    form mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 let popup = document.querySelector('.popup');
+let popup_mesto = document.querySelector('.popup-mesto');
 let Edit_button = document.querySelector('.profile-info__edit-button');
+let add_button = document.querySelector('.profile__add-button'); 
 let title=document.querySelector('.profile-info__title');
 let subtitle=document.querySelector('.profile-info__subtitle');
 let nameInput = document.querySelector('.popup__container-input_name_first');
@@ -10,6 +12,11 @@ let formElement = document.querySelector('.popup__container');
 Edit_button.addEventListener("click",OpenForm);
 close.addEventListener("click",CloseForm);
 formElement.addEventListener('submit', handleFormSubmit);
+add_button.addEventListener("click",OpenFormMesto);
+
+function OpenFormMesto() {
+    popup_mesto.classList.toggle('popup_opened');
+}
 
 function OpenForm() {
     popup.classList.toggle('popup_opened');

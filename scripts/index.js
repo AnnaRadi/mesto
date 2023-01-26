db31e6479783e0e2763c0dd545b962317ffa06ea
Anna Radionova <aradion0va@yandex.ru>
2023-01-26 12:20:55 +0300

    cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let popup = document.querySelector('.popup');
+let popup_pofile = document.querySelector('.popup-pofile');
 let popup_mesto = document.querySelector('.popup-mesto');
 let Edit_button = document.querySelector('.profile-info__edit-button');
 let add_button = document.querySelector('.profile__add-button'); 
@@ -6,26 +6,33 @@ let title=document.querySelector('.profile-info__title');
 let subtitle=document.querySelector('.profile-info__subtitle');
 let nameInput = document.querySelector('.popup__container-input_name_first');
 let jobInput = document.querySelector('.popup__container-input_about_you');
-let close = document.querySelector('.popup__container-close');
+let close_profile = document.querySelector('.popup__container-close_profile');
+let close_mesto = document.querySelector('.popup__container-close_mesto');
 let formElement = document.querySelector('.popup__container'); 
+const like=document.querySelectorAll('.element__group-title-like')
 
 Edit_button.addEventListener("click",OpenForm);
-close.addEventListener("click",CloseForm);
+close_profile.addEventListener("click",CloseForm);
 formElement.addEventListener('submit', handleFormSubmit);
 add_button.addEventListener("click",OpenFormMesto);
+close_mesto.addEventListener("click",CloseFormMesto);
 
 function OpenFormMesto() {
     popup_mesto.classList.toggle('popup_opened');
 }
 
 function OpenForm() {
-    popup.classList.toggle('popup_opened');
+    popup_pofile.classList.toggle('popup_opened');
     nameInput.value = title.textContent;
     jobInput.value = subtitle.textContent;
 }
 
+function CloseFormMesto() {
+    popup_mesto.classList.toggle('popup_opened');
+}
+
 function CloseForm() {
-    popup.classList.toggle('popup_opened');
+    popup_pofile.classList.toggle('popup_opened');
 }
 
 function handleFormSubmit (evt) {
@@ -35,4 +42,45 @@ function handleFormSubmit (evt) {
     popup.classList.toggle('popup_opened');
 }
 
+like.forEach(function(like){
+    like.addEventListener('click', function (evt) {
+        evt.target.classList.toggle('element__group-title-like_active');
+      });
 
+});
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const Container = document.querySelector('.elements');
+  initialCards.forEach(function(image, title){
+    const Template = document.querySelector('#element').content; //нашли Template
+    const userElement = Template.querySelector('.element').cloneNode(true);// клонируем содержимое Template (карточку)
+    userElement.querySelector('.element__image').src=initialCards.link;
+    userElement.querySelector('.element__title').textContent=initialCards.name;
+    Container.append(userElement);
+  });

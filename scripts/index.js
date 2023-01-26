8cbabefd4ca1be80f7346f43ae242aba326f0d8d
Anna Radionova <aradion0va@yandex.ru>
2023-01-26 20:43:37 +0300

    like

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,6 @@ let jobInput = document.querySelector('.popup__container-input_about_you');
 let close_profile = document.querySelector('.popup__container-close_profile');
 let close_mesto = document.querySelector('.popup__container-close_mesto');
 let formElement = document.querySelector('.popup__container'); 
-const like=document.querySelectorAll('.element__group-title-like')
 
 Edit_button.addEventListener("click",OpenForm);
 close_profile.addEventListener("click",CloseForm);
@@ -39,15 +38,10 @@ function handleFormSubmit (evt) {
     evt.preventDefault(); 
     title.textContent = nameInput.value;
     subtitle.textContent = jobInput.value;
-    popup.classList.toggle('popup_opened');
+    popup_pofile.classList.toggle('popup_opened');
 }
 
-like.forEach(function(like){
-    like.addEventListener('click', function (evt) {
-        evt.target.classList.toggle('element__group-title-like_active');
-      });
 
-});
 
 const initialCards = [
     {
@@ -77,10 +71,35 @@ const initialCards = [
   ];
 
   const Container = document.querySelector('.elements');
-  initialCards.forEach(function(image, title){
+  function createCards(create_title, create_images){
     const Template = document.querySelector('#element').content; //нашли Template
     const userElement = Template.querySelector('.element').cloneNode(true);// клонируем содержимое Template (карточку)
-    userElement.querySelector('.element__image').src=initialCards.link;
-    userElement.querySelector('.element__title').textContent=initialCards.name;
+    userElement.querySelector('.element__title').textContent=create_title;
+    userElement.querySelector('.element__image').src=create_images;
+    const like = Template.querySelectorAll('.element__group-title-like')
+    like.forEach(function(like){
+    like.addEventListener('click', function (evt) {
+      evt.target.classList.toggle('.element__group-title-like_active');
+      });
+});
     Container.append(userElement);
-  });
+  }
+  
+  initialCards.forEach(card => {
+    createCards(card.name, card.link)
+    });
+
+    //let title_mesto=document.querySelector('.element__title');
+    //let image_mesto=document.querySelector('.element__image');
+    //let titleInput = document.querySelector('.popup__container-input_name_mesto');
+    //let imageInput = document.querySelector('.popup__container-input_link_picture');
+    //let formElement_mesto = document.querySelector('.popup-mesto__container-card'); 
+    //formElement_mesto.addEventListener('submit', Newcard);
+    
+    //function Newcard(evt) {
+      //evt.preventDefault(); 
+      //const title_mesto=titleInput.value;
+      //const image_mesto.src=imageInput.value;
+      //const card=createCards({title:titleInput.value, image:imageInput.value});
+      //Container.append(card);
+ //}
